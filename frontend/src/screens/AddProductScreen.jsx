deff8a4fe1a523a95b4cd8fb4024bb4b9fe8b223
mariaaescribano <mariaaescribanoarce3@gmail.com>
2026-04-27 09:24:43 +0200

    cambios minimos

--- frontend/src/screens/AddProductScreen.jsx
+++ frontend/src/screens/AddProductScreen.jsx
@@ -24,7 +24,7 @@ const categoryOptions = [
 
 const previewImages = {
   Frutas: require("../../assets/images/comida/naranjas.webp"),
-  Verduras: require("../../assets/images/comida/verduras.jpeg"),
+  Verduras: require("../../assets/images/comida/verduras.webp"),
   Especias: require("../../assets/images/comida/pimenton.jpg"),
 };
 
